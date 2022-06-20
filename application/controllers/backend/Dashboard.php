1b35a5d7bd1a873b481c81e2ed264335694f9646
rabs123 <rajabarodas123@gmail.com>
2022-06-20 18:00:44 +0700

    Membuat form validasi login

---
+++ application/controllers/backend/Dashboard.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends MY_Controller 
+{
+	public function index()
+	{
+		view('backend/dashboard/index');
+	}
+
+	public function table()
+	{
+		view('backend/dashboard/table');
+	}
+}
