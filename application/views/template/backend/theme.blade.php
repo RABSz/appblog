e4dcb4fa6618bb1e8c2b0c5c017c705e8ae4c01e
rabs123 <rajabarodas123@gmail.com>
2022-06-24 22:14:58 +0700

    perubahan dan tambah fitur edit profile

--- application/views/template/backend/theme.blade.php
+++ application/views/template/backend/theme.blade.php
@@ -15,6 +15,7 @@
     
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
 
     <!-- Favicons -->
 <!--<link rel="apple-touch-icon" href="/docs/5.2/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
@@ -113,6 +114,12 @@
             <a class="nav-link" href="{{ site_url('backend/dashboard/table') }}">
               <span data-feather="file" class="align-text-bottom"></span>
               Table 
+            </a>
+          </li>
+					<li class="nav-item">
+            <a class="nav-link" href="{{ site_url('backend/user/profile') }}">
+						<i class="bi bi-person-circle" style="margin-right: 4px;"></i>
+              Update profile 
             </a>
           </li>
         </ul>
